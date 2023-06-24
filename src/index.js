69718006dca3876048242c8bbb550fcd80a60ec5
Twitteryzeus <bvlakkad@gmail.com>
2023-06-24 11:49:03 +0530

    add sequelize client

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const logger = require('./utils/logger');
+const { sequelize } = require('./models');
 
 const app = express();
 app.use(cors());
@@ -12,7 +13,29 @@ app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
-app.listen(config.port, () => {
-  logger.info('Server is started')
-  console.log(`Server is running on http://localhost:${config.port}/`);
-})
+// Authenticate sequelize connection here.
+sequelize
+  .authenticate()
+  .then(() => {
+    logger.info('Sequelize Authenticated Successfully');
+
+    // After authentication is done successfully, need to sync all the models.
+    sequelize
+      .sync()
+      .then(() => {
+        logger.info('Models synced successfully');
+
+        // After sync is completed. Start the express server.
+        app.listen(config.port, () => {
+          logger.info('Server is started')
+          console.log(`Server is running on http://localhost:${config.port}/`);
+        })
+      })
+      .catch((error) => {
+        logger.error(`ERROR > INDEX > SEQUELIZE SYNC > ${error?.message}`);
+      })
+  }).catch((error) => {
+    logger.error(`ERROR > INDEX > SEQUELIZE AUTHENTICATION > ${error?.message}`);
+  });
+
+
---
+++ src/models/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const { db: config } = require('../config');
+const db = {};
+
+const sequelize = new Sequelize(config.name, config.username, config.password, {
+  host: config.host,
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+});
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-9) === '.model.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
