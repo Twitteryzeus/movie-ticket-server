115ee50b3a1dc3cbf5aa82aa6242436b65254947
Twitteryzeus <bvlakkad@gmail.com>
2023-06-24 11:17:47 +0530

    add database credentials

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  port: +process.env.PORT
+  port: +process.env.PORT,
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME
+  }
 };
